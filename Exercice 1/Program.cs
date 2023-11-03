b875293e623a05c21115f5bfff6a659cbcdcbf8a
MonSeigneur03 <149824773+MonSeigneur03@users.noreply.github.com>
2023-11-03 20:12:14 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice 1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_1
+{
+    internal class Program
+    {
+        // Déclarations globabes
+        static char reponse;
+        static void saisie()
+        {
+            do
+            {
+                Console.Write("Avez vous un prix à saisir ? (O/N) : ");
+                reponse = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+            } while (reponse != 'O' && reponse != 'N');
+        }
+        static void Main(string[] args)
+        {
+            // Déclarations
+            float prix;
+            float total = 0;
+
+            // Demande de saisie d'un premier prix
+            saisie();
+
+            // Saisie d'un premier prix
+            if (reponse == 'O')
+            {
+                Console.WriteLine();
+                Console.Write("Saisissez un prix : ");
+                prix = float.Parse(Console.ReadLine());
+                total = total + prix;
+            }
+            // Demande de saisie d'un prix
+            while (reponse == 'O')
+            {
+                saisie();
+                // Saisie d'un prix
+                if (reponse == 'O')
+                {
+                    Console.WriteLine();
+                    Console.Write("Saisissez un prix : ");
+                    prix = float.Parse(Console.ReadLine());
+                    total = total + prix;
+                }
+                else
+                {
+                    Console.Write("Le total des prix est : " + total + " euros.");
+                    Console.ReadLine();
+                }
+            }
+
+
+        }
+    }
+}
